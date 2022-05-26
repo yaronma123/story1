062afc5d8baa226a45e5a34dbac4604bda08c8dc
yaronma123 <mairzik@gmail.com>
2022-05-26 11:19:49 +0300

    story1
    github

---
+++ story_content/user.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6J7jtZ7EJBt":
+        Script1();
+        break;
+      case "5j3XOLcbhf5":
+        Script2();
+        break;
+      case "6ZlKOVMHWZn":
+        Script3();
+        break;
+      case "5wzwWnB17fP":
+        Script4();
+        break;
+  }
+}
+
+function Script1()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script2()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script3()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
+function Script4()
+{
+  var els = document.getElementsByTagName('input');
+for (var i=0; i < els.length; i++) {
+ els[i].setAttribute("autocomplete", "off");
+}
+}
+
